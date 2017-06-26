286f8c721fc2b1481f8a8ff674fbeba87a982f3a
pycat <kkampardi@gmail.com>
2017-06-26 12:36:12 +0300

    Reset the base application project

---
+++ project/gallery/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from filer.fields.image import FilerImageField
+
+
+# Create your models here.
+
+
+class Gallery(models.Model):
+    title = models.CharField(max_length=200)
+    slice = models.SlugField(unique=True)
+
+    featured_image = FilerImageField(related_name="featured")
+
+    def __str__(self):
+        return self.name
